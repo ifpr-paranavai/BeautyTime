4abdb10a54d1d8c0b2a1d22d856054cd35320f0b
Gustavo Orelio <oreliogustavo@gmail.com>
2023-04-23 23:59:10 -0300

    Iniciando repositorio

---
+++ BeautyTimeBackEnd/src/main/java/com/gustavoorelio/beautytime/BeautytimeApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoorelio.beautytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautytimeApplication.class, args);
+	}
+
+}
---
+++ BeautyTimeBackEnd/src/test/java/com/gustavoorelio/beautytime/BeautytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoorelio.beautytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
