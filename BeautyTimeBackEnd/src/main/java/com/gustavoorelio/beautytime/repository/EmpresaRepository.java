1a94e9a8a91c177bace73976d1a17d31ac736525
Gustavo Orelio <oreliogustavo@gmail.com>
2023-08-05 23:27:05 -0300

    Removendo cadastro de Empresa

--- BeautyTimeBackEnd/src/main/java/com/gustavoorelio/beautytime/repository/EmpresaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gustavoorelio.beautytime.repository;
-
-import com.gustavoorelio.beautytime.model.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-}
