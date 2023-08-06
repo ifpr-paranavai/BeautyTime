ba428a98a887915b0c46221344091d6d92e16be9
Gustavo Orelio <oreliogustavo@gmail.com>
2023-08-06 00:21:26 -0300

    backend de serviço

---
+++ BeautyTimeBackEnd/src/main/java/com/gustavoorelio/beautytime/controller/ServicoController.java
@@ -0,0 +1,51 @@
+package com.gustavoorelio.beautytime.controller;
+
+import com.gustavoorelio.beautytime.model.Servico;
+import com.gustavoorelio.beautytime.service.ServicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/beautytime/servico")
+@CrossOrigin
+public class ServicoController {
+
+    @Autowired
+    private ServicoService servicoService;
+
+    @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
+    public List<Servico> buscarTodos(){
+        return servicoService.buscarTodos();
+    }
+
+    @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
+    public Servico inserir(@RequestBody Servico servico){
+        return servicoService.inserir(servico);
+    }
+
+    @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
+    public Servico alterar(@RequestBody Servico servico){
+        return servicoService.alterar(servico);
+    }
+
+    @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id){
+        servicoService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Servico> buscarPorId(@PathVariable("id") Long id){
+
+        return ResponseEntity.ok(servicoService.buscarPorId(id));
+    }
+
+}
---
+++ BeautyTimeBackEnd/src/main/java/com/gustavoorelio/beautytime/model/Servico.java
@@ -0,0 +1,33 @@
+package com.gustavoorelio.beautytime.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "servico")
+@Data
+public class Servico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private Integer tempoServico;
+
+    private String descricao;
+
+    private Double preco;
+
+    private Boolean status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCadastro;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+
+}
